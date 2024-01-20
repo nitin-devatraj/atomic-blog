0f764d599ddb043a1da73fac49cc9afa0db72565
nitin devatraj <nitindevatraj@protonmail.com>
2024-01-20 19:51:18 +0530

    implement Archive component

---
+++ src/components/archive/Archive.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { createRandomPost } from "../../utilities/utilities";
+
+export default function Archive({ onAddPost }) {
+  const [showArchive, setShowArchive] = useState(false);
+  const [posts] = useState(() =>
+    Array.from({ length: 15 }, () => createRandomPost())
+  );
+
+  return (
+    <aside>
+      <h2>Post archive</h2>
+      <button onClick={() => setShowArchive((s) => !s)}>
+        {showArchive ? "Hide archive posts" : "Show archive posts"}
+      </button>
+
+      {showArchive && (
+        <ul>
+          {posts.map((post, i) => (
+            <li key={i}>
+              <p>
+                <strong>{post.title}:</strong> {post.body}
+              </p>
+              <button onClick={() => onAddPost(post)}>Add as new post</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </aside>
+  );
+}
